c9a073d9e23a1ebfde9ecf9030a1a74542800d73
chixchix55 <jsegovich55@gmail.com>
2025-11-03 14:11:46 -0400

    Correo 4

--- send-test.php
+++ send-test.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require __DIR__ . 'PHPMailer-master/PHPMailer-master/src/PHPMailer.php';
-require __DIR__ . 'PHPMailer-master/PHPMailer-master/src/SMTP.php';
-require __DIR__ . 'PHPMailer-master/PHPMailer-master/src/Exception.php';
+require __DIR__ . '/PHPMailer-master/PHPMailer-master/src/PHPMailer.php';
+require __DIR__ . '/PHPMailer-master/PHPMailer-master/src/SMTP.php';
+require __DIR__ . '/PHPMailer-master/PHPMailer-master/src/Exception.php';
 use PHPMailer\PHPMailer\PHPMailer;
 $m = new PHPMailer(true);
 $m->isSMTP();
