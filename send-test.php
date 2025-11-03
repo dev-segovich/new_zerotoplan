b004cb4efb2b0d5d2bab20e81e0568d1025a4c54
chixchix55 <jsegovich55@gmail.com>
2025-11-03 14:29:04 -0400

    Correo 6

--- send-test.php
+++ send-test.php
@@ -14,7 +14,7 @@ $m->Password='5)}dQ&%jli4j!8bc';
 $m->SMTPSecure=PHPMailer::ENCRYPTION_SMTPS;
 $m->Port=465;
 $m->setFrom('no-reply@zerotoplan.com','Zero to Plan');
-$m->addAddress('jsegovich55@gmail.com');
+$m->addAddress('jesus.blondell@zerotoplan.com');
 $m->isHTML(true);
 $m->Subject='SMTP OK test';
 $m->Body='Funciona ✅';
